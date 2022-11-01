a1d2039cd7901b0d380a8e4fd8b6a5a4abd9a13c
shilpasy <shilpa.yadav@metropolia.fi>
2022-11-01 14:03:29 +0200

    necessary changes done in week-two

--- server1/index.js
+++ server1/index.js
@@ -1,22 +1,15 @@
-'use strict';
+"use strict";
 
-
-
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = 3000;
 
-
-
 let requestCounter = 0;
 
-
-
-app.use(express.static('public'));
-app.set('view engine', 'pug');
-
+app.use(express.static("public"));
+app.set("view engine", "pug");
 
 /*
 
@@ -28,47 +21,34 @@ app.get('/', (req, res) => {
 
 */
 
-
-
-app.get('/catinfo', (req, res) => {
-
+app.get("/catinfo", (req, res) => {
   const cat = {
-
     name: "Mayalu",
 
     birthdate: "2021-12-25",
 
     weight: 5,
-
   };
 
   res.json(cat);
-
-})
-
-
-
-app.get('/test', (request, response) => {
-    console.log('Someone is trying to test me');
-    requestCounter++;
-    //Exapmple of using pug
-    response.render('test', {
-        title: "Pug test page",
-        header1: "Pug test page",
-        header2: "Counter",
-        someText: "Page requested " + requestCounter + " times.",
-    });
-    //basic html as string
-    //response.send('<h1>test is not available</h1> <p>'
-
-    //+ requestCounter + '<p>');
-
 });
 
+app.get("/test", (request, response) => {
+  console.log("Someone is trying to test me");
+  requestCounter++;
+  //Exapmple of using pug
+  response.render("test", {
+    title: "Pug test page",
+    header1: "Pug test page",
+    header2: "Counter",
+    someText: "Page requested " + requestCounter + " times.",
+  });
+  //basic html as string
+  //response.send('<h1>test is not available</h1> <p>'
 
+  //+ requestCounter + '<p>');
+});
 
 app.listen(port, () => {
-
-  console.log(`Example app listening on port ${port}`)
-
+  console.log(`Example app listening on port ${port}`);
 });
