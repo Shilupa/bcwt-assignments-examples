5b61406de16d03ecbcb86fc2f3a8d4968ac30956
shilpasy <shilpa.yadav@metropolia.fi>
2022-11-15 14:21:04 +0200

    added validator example rule for user creation on server-side

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 "use strict";
 const userModel = require("../models/userModel");
+const { validationResult } = require("express-validator");
 
 const getUsers = async (req, res) => {
   const users = await userModel.getAllUsers(res);
@@ -20,30 +21,40 @@ const getUser = async (req, res) => {
 const createUser = async (req, res) => {
   console.log("Creating a new user:", req.body);
   const newUser = req.body;
-  const result = await userModel.addUser(newUser, res);
-  res.status(201).json({ userId: result });
+  if (newUser.role) {
+    //default user role (normal user)
+    newUser.role = 1;
+  }
+  const errors = validationResult(req);
+  console.log("validation errors", errors);
+  if (errors.isEmpty()) {
+    const result = await userModel.addUser(newUser, res);
+    res.status(201).json({ userId: result });
+  } else {
+    res.status(400).json({ message: 'user created failed' ,errors: errors.array() });
+  }
 };
 
-const modilfyUser = async(req, res) => {
+const modilfyUser = async (req, res) => {
   const userId = req.body;
-  if (req.params.userId){
+  if (req.params.userId) {
     user.id = req.params.userId;
   }
   const result = await userModel.updateUserById(user, res);
   if (result.affectedRows > 0) {
-    res.json({message: 'user modified' + user.id});
+    res.json({ message: "user modified" + user.id });
   } else {
-    res.status(404).json({message: 'nothing changed'});
+    res.status(404).json({ message: "nothing changed" });
   }
 };
-  
-const deleteUser = async(req, res) => {
+
+const deleteUser = async (req, res) => {
   const result = await userModel.deleteUserById(req.params.userId, res);
-   console.log('user deleted', result)
-  if (result.affectedRows > 0){
-    res.json({message: 'user deleted'});
+  console.log("user deleted", result);
+  if (result.affectedRows > 0) {
+    res.json({ message: "user deleted" });
   } else {
-    res.status(404).json({message: 'user was already deleted'});
+    res.status(404).json({ message: "user was already deleted" });
   }
 };
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,20 +3,19 @@
 
 const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const { body, validationResult } = require("express-validator");
+
+const userController = require("../controllers/userController");
 
 //TODO:  chain the router method calls
-router.get("/", userController.getUsers);
-
-router.get("/:userId", userController.getUser);
-
-router.post("/", userController.createUser);
-
-router.put("/user", (req, res) => {
-  console.log(req);
-  res.send("From this endpoint you can edit users.");
-  // TODO: Replace with controller and data model
-});
+router
+  .get("/", userController.getUsers)
+  .get("/:userId", userController.getUser)
+  .post("/", body("name").isLength({ min: 3 }), userController.createUser)
+  .put("/user", (req, res) => {
+    res.send("From this endpoint you can edit users.");
+    // TODO: Replace with controller and data model
+  });
 
 router.delete("/user", (req, res) => {
   res.send("From this endpoint you can delete users.");
