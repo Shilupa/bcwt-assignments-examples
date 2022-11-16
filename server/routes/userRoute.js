7fdc16e973618e1312029eaa60aad2ad0ba5f06d
shilpasy <shilpa.yadav@metropolia.fi>
2022-11-16 10:37:45 +0200

    add basic sanitization

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -12,15 +12,17 @@ router
   .get("/", userController.getUsers)
   .get("/:userId", userController.getUser)
   .post("/", 
-  body("name").isLength({ min: 3 }), 
-  body("email").isEmail(),
-  body("passwd").isLength({ min: 8 }),
+  body("name").isLength({ min: 3 }).trim().escape(), 
+  body("email").isEmail().normalizeEmail(),
+  body("passwd").isLength({ min: 8 }).trim(),
   userController.createUser)
   .put("/", (req, res) => {
     res.send("From this endpoint you can edit users.");
     // TODO: Replace with controller and data model
+    // TODO: add validators too
   })
   .delete("/", (req, res) => {
+    // TODO: Replace with controller and data model
     res.send("From this endpoint you can delete users.");
   });
 //router.put('TODO');
