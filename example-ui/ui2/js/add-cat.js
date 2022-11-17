4f740df0199013efdddec782853e44f3e4bc0c19
shilpasy <shilpa.yadav@metropolia.fi>
2022-11-18 00:38:54 +0200

    changed the text to date in modify-cat.html and made few minor changes in add-cat.html and add-cat.js

--- example-ui/ui2/js/add-cat.js
+++ example-ui/ui2/js/add-cat.js
@@ -1,20 +1,21 @@
-'use strict';
-const url = 'http://localhost:3000'; // change url when uploading to server
+"use strict";
+const url = "http://localhost:3000"; // change url when uploading to server
 
 // select existing html elements
-const addForm = document.querySelector('#addCatForm');
-const userList = document.querySelector('.add-owner');
+const addForm = document.querySelector("#addCatForm");
+const userList = document.querySelector(".add-owner");
 
 // create user options to <select>
 const createUserOptions = (users) => {
   // clear user list
-  userList.innerHTML = '';
+  console.log(users);
+  userList.innerHTML = "";
   users.forEach((user) => {
     // create options with DOM methods
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     option.value = user.user_id;
     option.innerHTML = user.name;
-    option.classList.add('light-border');
+    option.classList.add("light-border");
     userList.appendChild(option);
   });
 };
@@ -22,7 +23,7 @@ const createUserOptions = (users) => {
 // get users to form options
 const getUsers = async () => {
   try {
-    const response = await fetch(url + '/user');
+    const response = await fetch(url + "/user");
     const users = await response.json();
     createUserOptions(users);
   } catch (e) {
@@ -32,14 +33,14 @@ const getUsers = async () => {
 getUsers();
 
 // submit add cat form
-addForm.addEventListener('submit', async (evt) => {
+addForm.addEventListener("submit", async (evt) => {
   evt.preventDefault();
   const fd = new FormData(addForm);
   const fetchOptions = {
-    method: 'POST',
+    method: "POST",
     body: fd,
   };
-  const response = await fetch(url + '/cat', fetchOptions);
+  const response = await fetch(url + "/cat", fetchOptions);
   const json = await response.json();
   alert(json.message);
   //location.href = 'front.html';
