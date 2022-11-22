dc8a3abc31f89524884fbaf7430f856ac3b57c45
shilpasy <shilpa.yadav@metropolia.fi>
2022-11-23 00:02:04 +0200

    session started

--- state-management-example/app.js
+++ state-management-example/app.js
@@ -4,24 +4,49 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
+const user = {
+  username: 'foo',
+  password: 'bar'
+};
+//don't do this
+let loggedIn = true;
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(cookieParser());
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({extended: true})); // for form data
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+app.get('/secret', (req, res) => {
+  if (loggedIn) {
+    res.render('secret');
+  } else {
+    res.redirect('/form');
+  }
+});
+app.post('/login', (req, res) => {
+  // check for username/password match
+  console.log(req.body);
+  if (req.body.username == user.username && req.body.password == user.password){
+    loggedIn = true;//set session variable
+  } 
+  res.redirect('/secret');
 });
 
 app.get('/getCookie', (req, res) => {
   console.log(req.cookies);
   res.send('your color choice was: ' + req.cookies.color);
 });
-
 app.get('/deleteCookie', (req, res) => {
   res.clearCookie('color');
   res.send('Color cookie deleted.');
 });
-
 app.get('/setCookie/:color', (req, res) => {
   console.log(req.params.color);
   res.cookie('color', req.params.color);
