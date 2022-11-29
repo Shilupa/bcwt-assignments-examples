965d6482f65baa0becf74cb103ee8437df111763
shilpasy <shilpa.yadav@metropolia.fi>
2022-11-29 14:27:03 +0200

    add register endpoint, check user permissions for editing cat

--- server/models/catModel.js
+++ server/models/catModel.js
@@ -4,9 +4,9 @@ const promisePool = pool.promise();
 
 const getAllCats = async (res) => {
   try {
-    //const sql = "select * from wop_cat";
-    const sql = `Select wop_user.name  as ownername, wop_cat.name, cat_id, weight, owner, filename, birthdate from wop_cat 
-                 inner join wop_user on wop_cat.owner = wop_user.user_id`;
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const sql = 'SELECT cat_id, wop_cat.name, weight, owner, filename, birthdate, wop_user.name AS ownername ' +
+      'FROM wop_cat JOIN wop_user ON wop_cat.owner = wop_user.user_id';
     const [rows] = await promisePool.query(sql);
     return rows;
   } catch (e) {
@@ -17,11 +17,9 @@ const getAllCats = async (res) => {
 
 const getCatById = async (res, catId) => {
   try {
-    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
-    const sql =
-      "SELECT cat_id, wop_cat.name, weight, wop_user.name as ownername, filename, birthdate" +
-      " FROM wop_cat JOIN wop_user ON wop_cat.owner = wop_user.user_id WHERE wop_cat.cat_id = ?";
-    const [rows] = await promisePool.query(sql, [catId]);
+    const [rows] =
+      await promisePool.query("SELECT * FROM wop_cat WHERE cat_id = ?", [catId]);
+    console.log('getting cat', rows[0]);
     return rows[0];
   } catch (e) {
     console.error("error", e.message);
@@ -31,15 +29,9 @@ const getCatById = async (res, catId) => {
 
 const addCat = async (cat, res) => {
   try {
-    //console.log('addCat():', cat);
-    const sql = "INSERT INTO wop_cat VALUES (null, ?, ?, ?, ?, ?)";
-    const values = [
-      cat.name,
-      cat.weight,
-      cat.owner,
-      cat.filename,
-      cat.birthdate,
-    ];
+    //console.log('addCat():', cat)
+    const sql = 'INSERT INTO wop_cat VALUES (null, ?, ?, ?, ?, ?)';
+    const values = [cat.name, cat.weight, cat.owner, cat.filename, cat.birthdate];
     const [result] = await promisePool.query(sql, values);
     return result.insertId;
   } catch (e) {
@@ -48,37 +40,41 @@ const addCat = async (cat, res) => {
   }
 };
 
-const deleteCatById = async (catId, user, res) => {
-  console.log("model", catId, user);
-  let sqlQuery = "DELETE FROM wop_cat WHERE cat_id = ? AND owner = ?";
-  let params = [catId, user.user_id];
-
-  if (user.role === 0) {
-    (sqlQuery = "DELETE FROM wop_cat WHERE cat_id = ?"), (params = [catId]);
-  }
-
+const updateCatById = async (cat, user, res) => {
   try {
-    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
-    const [rows] = await promisePool.query(sqlQuery, params);
+    console.log(user, 'is modifying cat:', cat);
+    let sql, values;
+    // if admin user
+    if (user.role == 0) {
+      sql =
+        'UPDATE wop_cat SET name = ?, weight = ?, birthdate = ?, owner = ? ' +
+        'WHERE cat_id = ?';
+      values = [cat.name, cat.weight, cat.birthdate, cat.owner, cat.id];
+    } else {
+      sql =
+        'UPDATE wop_cat SET name = ?, weight = ?, birthdate = ? ' +
+        'WHERE cat_id = ? AND owner = ?';
+      values = [cat.name, cat.weight, cat.birthdate, cat.id, user.user_id];
+    }
+    const [rows] =
+      await promisePool.query(sql, values);
     return rows;
   } catch (e) {
     console.error("error", e.message);
-    res.status(500).send(e.message);
+    res.status(500).json({'error': e.message});
   }
 };
 
-const updateCatById = async (cat, res) => {
+const deleteCatById = async (catId, owner, res) => {
   try {
-    console.log("Modify cat:", cat);
-    const sql =
-      "UPDATE wop_cat SET name = ?, weight = ?, owner = ?, birthdate = ? " +
-      "WHERE cat_id = ?";
-    const values = [cat.name, cat.weight, cat.owner, cat.birthdate, cat.id];
-    const [rows] = await promisePool.query(sql, values);
+    const [rows] = await promisePool.query(
+      'DELETE FROM wop_cat WHERE cat_id = ? AND owner = ?',
+      [catId, owner]
+    );
     return rows;
   } catch (e) {
-    console.error("error", e.message);
-    res.status(500).json({ error: e.message });
+    console.error('error', e.message);
+    res.status(500).send(e.message);
   }
 };
 
@@ -86,6 +82,6 @@ module.exports = {
   getAllCats,
   getCatById,
   addCat,
-  deleteCatById,
   updateCatById,
+  deleteCatById,
 };
